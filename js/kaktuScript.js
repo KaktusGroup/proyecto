47167bc53df91c8c4fa639fadd0cb376830ea461
Kaktus Group <kaktus.soft.2019@gmail.com>
2019-10-24 04:18:06 -0300

    Cambio de plantilla
    Cambio de plantilla de web

--- js/kaktuScript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("#btn1").click(function() {
-    $("#leerMas").slideToggle();
-  });
-  $("#btn2").click(function() {
-    $("#leerMas2").slideToggle();
-  });
-});
